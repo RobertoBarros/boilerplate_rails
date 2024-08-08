525adc746572547f21ac701ef482e710f077ff0d
Roberto Barros <roberto.f.barros@gmail.com>
2024-08-08 00:47:34 -0300

    login/logout

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus";
 import Notification from "@stimulus-components/notification";
+import RevealController from "@stimulus-components/reveal";
 
 const application = Application.start();
 
 application.register("notification", Notification);
+application.register("reveal", RevealController);
 
 // Configure Stimulus development experience
 application.debug = false;
