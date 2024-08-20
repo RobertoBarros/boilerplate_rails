75e682c92dd180e39b8ed85469b99da373cdd434
Roberto Barros <roberto.f.barros@gmail.com>
2024-08-20 19:57:10 -0300

    lookbook of label_component

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
+  # Lookbook configuration
+  config.lookbook.project_name = "Componentes"
+  config.lookbook.preview_paths = ["test/components/previews/tailwind_form"]
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
