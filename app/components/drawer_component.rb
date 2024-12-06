f5b0e0897ce44c7434c2a984c7796fb18d7eb064
Roberto Barros <roberto.f.barros@gmail.com>
2024-12-06 14:06:16 -0300

    simplificação do componente de drawer

--- app/components/drawer_component.rb
+++ app/components/drawer_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DrawerComponent < ViewComponent::Base
+  include Turbo::FramesHelper # https://github.com/ViewComponent/view_component/issues/1099#issuecomment-996862141
+
   def initialize(title:, animate: true)
     @title = title
     @animate = animate
